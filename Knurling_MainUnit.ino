5a22876542c2a0bf1ca57a60b37af80aa6e9641c
mgko1996 <mgko_424@dongguk.edu>
2021-08-16 15:56:49 +0900

    8.16(1)

--- Knurling_MainUnit.ino
+++ Knurling_MainUnit.ino
@@ -3,26 +3,31 @@
 #include <ESP32SharpIR.h>
 #include <time.h>
 
-
+//포트 번호 정의(default 80)
 WiFiServer server(80);
 WebSocketServer webSocketServer;
 
+
+//Sharp IR Sensor 신호선 33번 연결
 ESP32SharpIR sensor( ESP32SharpIR::GP2Y0A21YK0F, 33);
 
 int count = 0;
 String machine_name = "LetPullDown";
 static int distance;
-static bool flag1=false;
-static bool flag2=false;
+static bool time_flag;
+static bool flag_up = false;
+static bool flag_down = false;
 static String data;
 
 const char* ntpServer = "pool.ntp.org";
 uint8_t timeZone = 9; 
 uint8_t summerTime = 0; // 3600 
 
-const char* ssid = "yourNetworkName";
-const char* password = "yourNetworkPassword";
+const char* ssid = "KT_GiGA_2G_Wave2_DC75";
+const char* password = "db89kc5373";
 
+
+// 시간 관련 구조체에서 현재 시간과 관련된 정보인 date 추출
 void printLocalTime() {
   struct tm timeinfo; 
   if(!getLocalTime(&timeinfo)){
@@ -39,7 +44,14 @@ void printLocalTime() {
 }
 
 void setup(){
+
+  //보드 레이트 115200 설정
   Serial.begin(115200);
+
+  //센서 filter 하이퍼파라미터 정의
+  sensor.setFilterRate(0.4f);
+
+  //와이파이 연결
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(100);
@@ -47,8 +59,14 @@ void setup(){
   }
   Serial.println("Connected to the WiFi network");
   Serial.println(WiFi.localIP());
+  
+  //시간 재설정 및 정보 가져오기
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  
+  //esp32 서버 시작
   server.begin()
   delay(500);
+  time_flag = true
 }
 
 void loop(){
@@ -58,27 +76,30 @@ void loop(){
 
     while (client.connected()) {
       data = webSocketServer.getData();
-      if(data == "ture"){
-        printLocalTime();
+      if(data == "true"){
+        if(time_flag==true){
+          printLocalTime();
+          time_flag=false;
+        }
         distance = sensor.getDistance();
         webSocket.Server.sendData(data);
         if (distance<=20){
-          flag1=true;
-        } //플레이트 까지의 거리가 20cm 를 넘을 때, flag1을 true로 설정.
+          flag_up=true;
+        } //플레이트 까지의 거리가 20cm 를 넘을 때, flag_up을 true로 설정.
         
-        if ((flag1==true) && (40<=distance)){
-          flag2=true;
-        } //플레이트 까지의 거리가 40cm 를 넘을 때, flag2를 true로 설정.
+        if ((flag_up==true) && (40<=distance)){
+          flag_down=true;
+        } //플레이트 까지의 거리가 40cm 를 넘을 때, flag_down를 true로 설정.
   
-        if ((flag1==true) && (flag2==true))//flag1과 flag2가 모두 true일 때, count를 증가시킴.
+        if ((flag_up==true) && (flag_down==true))//flag_up과 flag_down가 모두 true일 때, count를 증가시킴.
         {
           count+=1;
           Serial.print(" Count  :: ");
           Serial.print(count);
           Serial.println();   
       
-          flag1=false; //flag1과 flag2는 false로 초기화
-          flag2=false;
+          flag_up=false; //flag_up과 flag_down는 false로 초기화
+          flag_down=false;
       
           Serial.println("ESP32 send data to TCP Client");
           webSocket.Server.sendData("운동 일자 : " + date + "운동 기구" + machine_name + "횟수 : " + String(count));
@@ -87,8 +108,9 @@ void loop(){
       }
       else{
         count = 0;
-        flag1 = false;
-        flag2 = false;
+        flag_up = false;
+        flag_down = false;
+        time_flag = true;
       }
     }
     Serial.println("The client disconnected");
